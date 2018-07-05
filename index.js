dab083c5eeda5e0e9699767a20101ba51961b60e
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-07-05 16:31:32 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const port = 8000;
+
+require('./app/routes')(app, {});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
